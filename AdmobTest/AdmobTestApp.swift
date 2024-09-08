d455f393f46ce7980978aa2720af0c47a0a5ed6c
k4zyn3 <k4zyn3@gmail.com>
2024-09-08 20:58:21 +0900

    Initial Commit

---
+++ AdmobTest/AdmobTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdmobTestApp.swift
+//  AdmobTest
+//
+//  Created by 湊日利 on 2024/09/08.
+//
+
+import SwiftUI
+
+@main
+struct AdmobTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
